31ed1c726fb47173ac70c655b5a3e8eb98a92bbb
Daniel <danielpqb@gmail.com>
2022-06-14 18:04:13 -0300

    Game is done. Trying to delay for 1s.

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ class Card {
     constructor({
         id,
         type,
-        backImageSrc
+        backImageSrc,
+        flipped = false
     }) {
         this.id = id
         this.type = type
         this.backImageSrc = backImageSrc
+        this.flipped = flipped
     }
 
     getRndType() {
@@ -46,7 +48,7 @@ class Card {
         //Create backImage as <img>
         const back = document.createElement('img')
         back.src = this.backImageSrc
-        back.className = 'backImage'
+        back.className = 'backImage hide'
 
         //Create cardBox as <div>
         const card = document.createElement('div');
@@ -63,21 +65,63 @@ class Card {
         //Put cardBox inside board
         board.appendChild(card)
     }
+
+    flip() {
+        //Is first choice?
+        if (isSecond === false) {
+            this.flipped = true
+            document.getElementById(this.id).querySelectorAll('img').forEach((img) => { img.classList.toggle('hide') })
+            isSecond = true
+        }
+        //Is second choice?
+        else {
+            //Chose same type (cards match)
+            if (this.type === lastCardsFlipped[0].type) {
+                this.flipped = true
+                document.getElementById(this.id).querySelectorAll('img').forEach((img) => { img.classList.toggle('hide') })
+            }
+            //Chose different types (cards do not match)
+            else {
+                //Flip card so the player can see what's inside
+                document.getElementById(this.id).querySelectorAll('img').forEach((img) => { img.classList.toggle('hide') })
+
+                //Unflip cards that didn't match after 1 second
+                this.flipped = false
+                lastCardsFlipped[0].flipped = false
+                //setTimeout(() => {
+                    document.getElementById(this.id).querySelectorAll('img').forEach((img) => { img.classList.toggle('hide') })
+                    document.getElementById(lastCardsFlipped[0].id).querySelectorAll('img').forEach((img) => { img.classList.toggle('hide') })
+                //}, 1000)
+            }
+            isSecond = false
+        }
+    }
 }
 
 function askCardsQnty() {
     let n = prompt('Com quantas cartas você quer jogar?')
-    n = Number(n)
-    while (isNaN(n) || n % 2 != 0 || n <= 0) {
-        alert('O valor digitado deve ser um número múltiplo de 2 !!!')
+    while (isNaN(n) || n % 2 != 0 || n < 4 || n > 200) {
+        if (n === null) { return 4 }
+        alert('| Regras do Jogo |\nO número de cartas deve ser:\n1) Um número múltiplo de 2.\n2) No mínimo 4.\n3) No máximo 200.')
         n = prompt('Com quantas cartas você quer jogar?')
     }
     return n
 }
 
 function selectCard() {
-    //this.id
+    cards.forEach((card) => {
+        if (this.id == card.id && card.flipped === false) {
+            card.flip()
+            lastCardsFlipped.unshift(card)
+        }
+    })
 }
+
+//Boolean that tells if player is flipping the second card
+let isSecond = false
+
+//Store the last cards flipped
+const lastCardsFlipped = []
 
 //Ask how many cards will be instanciated
 const cardsRequested = askCardsQnty()
@@ -90,7 +134,7 @@ const cards = []
 
 //Instanciate half qnty of cards
 for (let i = 1; i <= cardsRequested / 2; i++) {
-    cards.push(new Card({ id: i, }))
+    cards.push(new Card({ id: i }))
 }
 
 //Give a type to each card
